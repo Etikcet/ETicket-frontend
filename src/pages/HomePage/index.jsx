4fcdfe68b3c0a95648b8196628f7ec15d6074f82
Malitjh-19 <malith.19@cse.mrt.ac.lk>
2022-05-29 19:31:42 +0530

    navbar created & simple home page designed

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 
-export default function SignIn() {
+import HomeNavigationBar from "../../components/HomeNavigationBar";
+import BackgroundImage from "./background.jpg";
+
+
+
+export default function Home() {
   return (
     <div>
-      <h1>Signin</h1>
+      <div>
+        <HomeNavigationBar />
+        <p>Your partner in booking</p>
+        <h2>It's time to</h2>
+        <h1>Discover</h1>
+        <p>find and book your seat</p>
+      </div>
+
+      <h1
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Popular Routes
+      </h1>
     </div>
   );
 }
