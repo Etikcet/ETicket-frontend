6ebdd4b461df99a73a6a330c31d8c27694e6c584
Malitjh-19 <malith.19@cse.mrt.ac.lk>
2022-05-30 10:33:20 +0530

    footer component created

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 
 import HomeNavigationBar from "../../components/HomeNavigationBar";
+import Footer from "../../components/Footer";
 
 export default function Home() {
   return (
-    document.title='Home page',
-    <div>
+    (document.title = "Home page"),
+    (
       <div>
-        <HomeNavigationBar />
-        <p>Your partner in booking</p>
-        <h2>It's time to</h2>
-        <h1>Discover</h1>
-        <p>find and book your seat</p>
-      </div>
+        <div>
+          <HomeNavigationBar />
+          <p>Your partner in booking</p>
+          <h2>It's time to</h2>
+          <h1>Discover</h1>
+          <p>find and book your seat</p>
+        </div>
 
-      <p
-        style={{
-          textAlign: "center",
-          fontSize: 30,
-          fontStyle: "italic",
-        }}
-      >
-        Popular Routes
-      </p>
-    </div>
+        <p
+          style={{
+            textAlign: "center",
+            fontSize: 30,
+            fontStyle: "italic",
+          }}
+        >
+          Popular Routes
+        </p>
+        <Footer />
+      </div>
+    )
   );
 }
