7288849b0f9fa811f4f52611c3d013a137400c75
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2022-05-30 14:55:31 +0530

    signup page modified

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,107 +1,93 @@
 import React from "react";
 import Logo from "../../components/Logo";
-import SignUpImageDiv from "../../components/SignUpImage";
 import TextField from "@mui/material/TextField";
 import WhiteSpace from "../../components/WhiteSpace";
+import { styled } from "@mui/system";
 import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
+import SIGNUP_IMAGE from "../../assets/signup-image.svg";
+import { Stack } from "@mui/material";
+import HeightBox from "../../components/HeightBox";
+
+const CustomTextField = styled(TextField)({
+  width: 450,
+});
 
 export default function SignUp() {
-  const signUpFormStyle = {
-    padding: "1%",
-    top: "10%",
-    position: "absolute",
-    width: "30%",
-    height: "80%",
-    left: "60%",
-    // backgroundColor: "blue",
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-    // fontSize: "10px",
-  };
   return (
-    <div>
+    <div style={{ maxWidth: 1280, marginLeft: "auto", marginRight: "auto" }}>
       <Logo />
-      <SignUpImageDiv />
-      <div style={signUpFormStyle}>
-        <form action="">
-          <h2 style={{ top: "0px" }}>Welcome to Eticket!</h2>
-          <TextField
-            label="Name"
-            variant="outlined"
-            size="small"
-            color="secondary"
-            fullWidth
-            // style={{ height: 10 }}
-            InputProps={{ style: { fontSize: 10 } }}
-            InputLabelProps={{ style: { fontSize: 10 } }}
-          />
-          <WhiteSpace />
-          <TextField
-            label="Email(Username)"
-            variant="outlined"
-            size="small"
-            color="secondary"
-            type="email"
-            fullWidth
-            InputProps={{ style: { fontSize: 10 } }}
-            InputLabelProps={{ style: { fontSize: 10 } }}
-          />
-          <WhiteSpace />
-          <TextField
-            label="Phone Number"
-            variant="outlined"
-            size="small"
-            color="secondary"
-            // type="number"
-            fullWidth
-            InputProps={{ style: { fontSize: 10 } }}
-            InputLabelProps={{ style: { fontSize: 10 } }}
-          />
-          <WhiteSpace />
-          <TextField
-            label="Password"
-            variant="outlined"
-            size="small"
-            color="secondary"
-            type="password"
-            fullWidth
-            InputProps={{ style: { fontSize: 10 } }}
-            InputLabelProps={{ style: { fontSize: 10 } }}
-          />
-          <WhiteSpace />
-          <TextField
-            label="Confirm Password"
-            variant="outlined"
-            size="small"
-            color="secondary"
-            type="password"
-            fullWidth
-            InputProps={{ style: { fontSize: 10 } }}
-            InputLabelProps={{ style: { fontSize: 10 } }}
-          />
-          <WhiteSpace />
-          <Button
-            type="submit"
-            color="secondary"
-            size="small"
-            variant="contained"
-            fullWidth
-            InputProps={{ style: { fontSize: 10 } }}
-            InputLabelProps={{ style: { fontSize: 10 } }}
-          >
-            Sign Up
-          </Button>
-        </form>
-        <WhiteSpace />
-        <div style={{ fontSize: 10 }}>
-          Already have an account?{" "}
-          <Link href="#" underline="hover">
-            {"Sign In"}
-          </Link>
+      <HeightBox height={50} />
+      <Stack direction="row" spacing={15}>
+        <div>
+          <img src={SIGNUP_IMAGE} alt="" style={{ width: "40vw" }} />
         </div>
-      </div>
+        <div>
+          <form action="">
+            <h2 style={{ fontSize: 48, fontFamily: "Lato", margin: 0 }}>
+              Welcome to Eticket!
+            </h2>
+            <p style={{ color: "rgba(0,0,0,0.5)" }}>
+              Fill these details to sign up
+            </p>
+            <Stack direction="column" spacing={2}>
+              <CustomTextField
+                label="Name"
+                variant="outlined"
+                color="secondary"
+              />
+
+              <CustomTextField
+                label="Email or Username"
+                variant="outlined"
+                color="secondary"
+                type="email"
+              />
+
+              <CustomTextField
+                label="Phone Number"
+                variant="outlined"
+                color="secondary"
+              />
+
+              <CustomTextField
+                label="Password"
+                variant="outlined"
+                color="secondary"
+                type="password"
+              />
+
+              <CustomTextField
+                label="Confirm Password"
+                variant="outlined"
+                color="secondary"
+                type="password"
+              />
+
+              <Button
+                type="submit"
+                color="secondary"
+                variant="contained"
+                size="large"
+                fullWidth
+                InputProps={{ style: { fontSize: 10 } }}
+                InputLabelProps={{ style: { fontSize: 10 } }}
+              >
+                Sign Up
+              </Button>
+            </Stack>
+          </form>
+          <HeightBox height={15} />
+          <div style={{ fontSize: 15, width: 450 }}>
+            <Stack direction="row" justifyContent="center" spacing={1}>
+              <p style={{ margin: 0 }}>Already have an account?</p>
+              <Link href="#" underline="hover" color="black">
+                Sign In
+              </Link>
+            </Stack>
+          </div>
+        </div>
+      </Stack>
     </div>
   );
 }
