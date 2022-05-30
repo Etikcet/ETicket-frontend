6a706bbce27bb4f9cc204c3fb84c1bbf5846c139
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2022-05-30 12:17:25 +0530

    Added redux toolkit for the state management

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,2 +1,13 @@
+// Constants for backend integration
 export const HOST_URL = "https://eticketbackend.azurewebsites.net/api";
 export const TOKEN_KEY = "ETICKET-TOKEN";
+
+// Constants for user logging
+export const USER_LOGGING_REQUEST = "USER_LOGGING_REQUEST";
+export const USER_LOGGING_SUCCESFULL = "USER_LOGGING_SUCCESFULL";
+export const USER_LOGGING_FAILED = "USER_LOGGING_FAILED";
+
+// Constants for user registration
+export const USER_REGISTRATION_REQUEST = "USER_REGISTRATION_REQUEST";
+export const USER_REGISTRATION_SUCCESFULL = "USER_REGISTRATION_SUCCESFULL";
+export const USER_REGISTRATION_FAILED = "USER_REGISTRATION_FAILED";
