84f5cc7a79bbc208f6e6d551e601a26be817a637
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2022-05-30 14:24:46 +0530

    theme added

---
+++ src/utils/theme.jsx
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#9C27B0",
+    },
+    secondary: {
+      main: "#fff",
+    },
+  },
+});
+
+export default theme;
