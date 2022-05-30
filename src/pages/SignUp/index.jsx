39886abef5d065ab06fc4bb43a7aee57a59a250c
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2022-05-30 15:18:26 +0530

    validation added

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,16 +1,45 @@
 import React from "react";
 import Logo from "../../components/Logo";
 import TextField from "@mui/material/TextField";
-import WhiteSpace from "../../components/WhiteSpace";
+import { Formik } from "formik";
 import { styled } from "@mui/system";
 import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
 import SIGNUP_IMAGE from "../../assets/signup-image.svg";
 import { Stack } from "@mui/material";
 import HeightBox from "../../components/HeightBox";
+import * as Yup from "yup";
 
 const CustomTextField = styled(TextField)({
   width: 450,
+});
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required().label("Name"),
+  userName: Yup.string().required().label("User Name").min(3).max(36),
+  phoneNumber: Yup.number().required().label("Phone Number").max(10),
+  password: Yup.string()
+    .required()
+    .min(8)
+    .max(15)
+    .label("Password")
+    .matches(/\d+/, "Password should contain at least one number")
+    .matches(
+      /[a-z]+/,
+      "Password should contain at least one lowercase character"
+    )
+    .matches(
+      /[A-Z]+/,
+      "Passoword should contain at least one uppercase character"
+    )
+    .matches(
+      /[!@#$%^&*()-+]+/,
+      "Password should contain at least one special character"
+    ),
+  confirmPassword: Yup.string()
+    .required()
+    .label("Confirm Password")
+    .oneOf([Yup.ref("password"), null], "Passwords must match"),
 });
 
 export default function SignUp() {
@@ -31,50 +60,89 @@ export default function SignUp() {
               Fill these details to sign up
             </p>
             <Stack direction="column" spacing={2}>
-              <CustomTextField
-                label="Name"
-                variant="outlined"
-                color="secondary"
-              />
-
-              <CustomTextField
-                label="Email or Username"
-                variant="outlined"
-                color="secondary"
-                type="email"
-              />
-
-              <CustomTextField
-                label="Phone Number"
-                variant="outlined"
-                color="secondary"
-              />
-
-              <CustomTextField
-                label="Password"
-                variant="outlined"
-                color="secondary"
-                type="password"
-              />
-
-              <CustomTextField
-                label="Confirm Password"
-                variant="outlined"
-                color="secondary"
-                type="password"
-              />
-
-              <Button
-                type="submit"
-                color="secondary"
-                variant="contained"
-                size="large"
-                fullWidth
-                InputProps={{ style: { fontSize: 10 } }}
-                InputLabelProps={{ style: { fontSize: 10 } }}
+              <Formik
+                initialValues={{
+                  name: "",
+                  userName: "",
+                  password: "",
+                  confirmPassword: "",
+                  phoneNumber: "",
+                }}
+                onSubmit={(values) => {
+                  // Validation success and needs to call backend
+                }}
+                validationSchema={validationSchema}
               >
-                Sign Up
-              </Button>
+                {(formikProps) => {
+                  const { errors, handleSubmit, handleChange, touched } =
+                    formikProps;
+
+                  return (
+                    <React.Fragment>
+                      <CustomTextField
+                        label="Name"
+                        variant="outlined"
+                        color="secondary"
+                        error={errors.name && touched.name}
+                        helperText={errors.name || ""}
+                        onChange={(event) => handleChange("name")(event)}
+                      />
+
+                      <CustomTextField
+                        label="Username"
+                        variant="outlined"
+                        color="secondary"
+                        error={errors.userName && touched.userName}
+                        helperText={errors.userName || ""}
+                        onChange={(event) => handleChange("userName")(event)}
+                      />
+
+                      <CustomTextField
+                        label="Phone Number"
+                        variant="outlined"
+                        color="secondary"
+                        error={errors.phoneNumber && touched.phoneNumber}
+                        helperText={errors.phoneNumber || ""}
+                        onChange={(event) => handleChange("phoneNumber")(event)}
+                      />
+
+                      <CustomTextField
+                        label="Password"
+                        variant="outlined"
+                        color="secondary"
+                        type="password"
+                        error={errors.password && touched.password}
+                        helperText={errors.password || ""}
+                        onChange={(event) => handleChange("password")(event)}
+                      />
+
+                      <CustomTextField
+                        label="Confirm Password"
+                        variant="outlined"
+                        color="secondary"
+                        type="password"
+                        error={
+                          errors.confirmPassword && touched.confirmPassword
+                        }
+                        helperText={errors.confirmPassword || ""}
+                        onChange={(event) =>
+                          handleChange("confirmPassword")(event)
+                        }
+                      />
+
+                      <Button
+                        type="submit"
+                        color="secondary"
+                        variant="contained"
+                        size="large"
+                        onClick={handleSubmit}
+                      >
+                        Sign Up
+                      </Button>
+                    </React.Fragment>
+                  );
+                }}
+              </Formik>
             </Stack>
           </form>
           <HeightBox height={15} />
