dd6f71ab1ed259ccd194dd7f8c49931c7b2bf4ef
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2022-05-31 14:03:31 +0530

    Signin finished

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,3 +1,5 @@
 // Constants for backend integration
 export const HOST_URL = "https://eticketbackend.azurewebsites.net/api";
 export const TOKEN_KEY = "ETICKET-TOKEN";
+
+export const ETICKET_USER_DETAILS = "ETICKET_USER_DETAILS";
