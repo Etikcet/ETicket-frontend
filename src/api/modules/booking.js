4f8779b674ba6a4038a20529886cbfeda235dd23
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2022-06-06 12:24:18 +0530

    booking status update api integrated

--- src/api/modules/booking.js
+++ src/api/modules/booking.js
@@ -5,4 +5,7 @@ export default {
   getUserBookings() {
     return resolver(axiosClient.get("/booking/mybookings"));
   },
+  updateBookingStatus(id) {
+    return resolver(axiosClient.get("/booking/update/" + id));
+  },
 };
